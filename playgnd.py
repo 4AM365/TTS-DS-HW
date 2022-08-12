f7119d19ee3a620286328307a03c55d5983cfaf9
4AM365 <wtcraig@live.com>
2022-08-11 17:42:48 -0700

    more progress

--- playgnd.py
+++ playgnd.py
@@ -1,6 +1,3 @@
-rollNumber = [47, 64, 69, 37, 76, 83, 95, 97]
-sampleDict ={'Zach':47, 'Emma':69, 'Kelly':76, 'Jason':97}
-
-rollNumber = [i for i in rollNumber if i in sampleDict.values()]
-
-print(rollNumber)
+print("enter 3 ages as digits separated by a space")
+age1 = int(input("Enter age: "))
+print(type(age1))
