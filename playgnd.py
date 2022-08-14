c62eafcfc85baef4f721b4d4f273da6edb64a1ff
4AM365 <wtcraig@live.com>
2022-08-13 20:50:46 -0700

    more hw

--- playgnd.py
+++ playgnd.py
@@ -1,3 +1,9 @@
-print("enter 3 ages as digits separated by a space")
-age1 = int(input("Enter age: "))
-print(type(age1))
+N = int(input("Enter a number: "))
+if(N % 2 != 0):
+    print("weird")
+if(N % 2 == 0 & 2 <= N <= 5):
+    print("Not Weird")
+if(N % 2 == 0 & 6 <= N <= 20):
+    print("Weird")
+if(N % 2 == 0 & N >= 20):
+    print("Weird")
